397a37387093cc6a640b3df8ad25e5ce35200db5
g14wx <carlos.hernandez3@catolica.edu.sv>
2022-05-25 05:59:23 -0600

    feat: :lipstick: CU-2yrwf7k Add splash screen only to android

--- android/app/src/main/kotlin/com/dnaapps/sv/penny_saver/MainActivity.kt
+++ android/app/src/main/kotlin/com/dnaapps/sv/penny_saver/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dnaapps.sv.penny_saver
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
